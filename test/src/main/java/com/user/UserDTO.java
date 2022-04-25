a33843293ce9fd0df36c1feeedb76694ef0d1cda
mglee <mglee@MG-Laptop>
2022-04-25 15:05:03 +0900

    첫 번째 테스트

---
+++ test/src/main/java/com/user/UserDTO.java
@@ -0,0 +1,14 @@
+package com.user;
+
+public class UserDTO {
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+}
